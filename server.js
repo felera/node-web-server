4517c5cfdf2c8dea128afb9210db04fac2550af5
francisco <elcofla46@hotmail.com>
2017-11-26 22:03:44 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials/');
+app.set('view engine','hbs');
+
+app.use((req,res,next)=>{
+    var now = new Date().toString();
+    var log = `${now} : ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log',log + '\n', (error)=>{
+        if(error){
+            console.log('Unable to append to file');
+        }
+    });
+    next();
+});
+
+// app.use((req, res)=>{
+//     res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', ()=>{
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt',(text)=>{
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res)=>{
+    res.render('home.hbs', {
+        pageTitle:'Home page',
+        welcomeMessage:'Hola Express!!!'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle:'About page'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+       errorMessage:'Sorry...bad request'
+    });
+});
+
+
+app.listen(3000);
